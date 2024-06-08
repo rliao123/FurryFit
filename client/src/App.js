150b3f8efa35287333b02c792e743c6ca89ce3dc
rliao123 <92598518+rliao123@users.noreply.github.com>
2024-06-07 20:17:14 -0700

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { useNavigate, useLocation, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+
+function App() {
+  const action = useNavigate();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    // eslint-disable-next-line default-case
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+  );
+}
+
+export default App;
